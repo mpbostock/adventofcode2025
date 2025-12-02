f737de67dcf207609c5c34115d3be131d3b3eb0e
Mike Bostock <17684266+mpbostock@users.noreply.github.com>
2025-12-02 23:34:19 +0000

    Day 2 tweaks

--- src/main/kotlin/mpbostock/Day02.kt
+++ src/main/kotlin/mpbostock/Day02.kt
@@ -2,6 +2,7 @@ package mpbostock
 
 object Day02 {
   data class ProductIdRange(val range: LongRange) {
+
     val invalidIds: List<Long>
       get() = range.map { it.toString() }.filter { it.length % 2 == 0 }.filter {
         val half = it.length / 2
@@ -11,22 +12,21 @@ object Day02 {
       }.map { it.toLong() }
 
     val invalidIdsSequences: List<Long>
-      get() {
-        return range.map { it.toString() }.filter {
-          val half = it.length / 2
-          (1 .. half).any { i ->
-            it.take(i).repeat(it.length / i) == it
-          }
-        }.map { it.toLong() }
+      get() = range.map { it.toString() }.filter {
+        val half = it.length / 2
+        (1..half).any { i ->
+          it.take(i).repeat(it.length / i) == it
+        }
+      }.map { it.toLong() }
 
-      }
     companion object {
       fun fromString(range: String): ProductIdRange {
         val split = range.split("-")
-        return ProductIdRange(split[0].toLong() .. split[1].toLong())
+        return ProductIdRange(split[0].toLong()..split[1].toLong())
       }
     }
   }
+
   fun partOne(input: List<String>): Long {
     val productIdRanges = input[0].split(",").map { ProductIdRange.fromString(it) }
     return productIdRanges.sumOf { it.invalidIds.sum() }
